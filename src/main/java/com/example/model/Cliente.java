fb6a83995bf0b9819a6fc9df406c0ce140e23747
Douglas <douglasmariano.com@gmail.com>
2020-11-11 19:29:52 -0300

    Vendedor e cliente adicionado

---
+++ src/main/java/com/example/model/Cliente.java
@@ -0,0 +1,73 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="pcclient")
+public class Cliente {
+	
+	@Id
+    @Column(name = "codcli")
+	private long codigoCliente;
+	
+	
+	@Column(name = "cliente")
+	private String nomeCliente;
+	
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(long codigoCliente, String nomeCliente) {
+		super();
+		this.codigoCliente = codigoCliente;
+		this.nomeCliente = nomeCliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigoCliente ^ (codigoCliente >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (codigoCliente != other.codigoCliente)
+			return false;
+		return true;
+	}
+
+	public long getCodigoCliente() {
+		return codigoCliente;
+	}
+
+	public void setCodigoCliente(long codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	
+	
+	
+	
+
+}
