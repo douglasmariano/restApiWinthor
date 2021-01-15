54319ea6531ced9644ea014e100d98adbc2f0d6d
Douglas <douglasmariano.com@gmail.com>
2021-01-15 18:17:39 -0300

    alterado tabpedidocontroller para fazer put de dataChegada e total

--- src/main/java/com/example/controller/TabPedidoController.java
+++ src/main/java/com/example/controller/TabPedidoController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.controller.payloads.PedidoPayload;
@@ -34,10 +38,10 @@ import com.example.services.TabPedidoService;
 public class TabPedidoController {
 	@Autowired
 	private TabPedidoRepository tabPedidoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private TabPedidoService tabPedidoService;
 
@@ -46,65 +50,77 @@ public class TabPedidoController {
 			throws ResourceNotFoundException {
 		List<PedidoPayload> pedido = null;
 		if (filter != null && !StringUtils.isEmpty(filter.getNumped())) {
-			pedido = tabPedidoService.findById(filter.getNumped());	
-		} else if (filter != null && (filter.getDataPedidoDe() != null || filter.getDataPedidoAte() != null || filter.getNomeCliente() != null || filter.getNomeVendedor() != null)) {
-			pedido = tabPedidoService.findByFiltro(filter);	
+			pedido = tabPedidoService.findById(filter.getNumped());
+		} else if (filter != null && (filter.getDataPedidoDe() != null || filter.getDataPedidoAte() != null
+				|| filter.getNomeCliente() != null || filter.getNomeVendedor() != null)) {
+			pedido = tabPedidoService.findByFiltro(filter);
 		} else {
 			pedido = tabPedidoService.findAll();
 		}
-		
+
 		return ResponseEntity.ok().body(pedido);
 	}
-	
-	
-	
+
 	@GetMapping("/tabpedidos")
 	public ResponseEntity<List<PedidoPayload>> getAllTabPedidos() {
 		List<PedidoPayload> resultado = tabPedidoService.findAll();
 		return ResponseEntity.ok().body(resultado);
-		
-	}	
-	
+
+	}
+
 	@PostMapping("/tabpedidos")
 	public TabPedido createTabPedido(@Valid @RequestBody TabPedido tabPedido) {
+		System.out.println("tabPedidoR");
 		return tabPedidoRepository.save(tabPedido);
+	}
+	
+	@PutMapping("/tabpedidos/marcar_chegada/{numped}")
+	public ResponseEntity<TabPedido> marcarChegada(@PathVariable(value = "numped") BigDecimal numped) throws ResourceNotFoundException {
+		TabPedido tabPedido = tabPedidoRepository.findById(numped)
+				.orElseThrow(() -> new ResourceNotFoundException("Pedido não encontrado com esse Numped :: " + numped));
+		
+		tabPedido.setDatachegadacli(new Date());
+
+		final TabPedido updateTabPedido = tabPedidoRepository.save(tabPedido);
+		return ResponseEntity.ok(updateTabPedido);
 	}
 
 	@PutMapping("/tabpedidos/{numped}")
-	   public ResponseEntity <TabPedido> updateTabPedido(@PathVariable(value = "numped") BigDecimal numped,			  
-			@Valid @RequestBody TabPedido tabPedidoDetails) throws ResourceNotFoundException{
-		  	TabPedido tabPedido = tabPedidoRepository.findById(numped).orElseThrow(() -> new ResourceNotFoundException("Pedido não encontrado com esse Numped :: "+ numped));
-			tabPedido.setCodfuncbalcao(tabPedidoDetails.getCodfuncbalcao());
-			tabPedido.setCodfuncpacote(tabPedidoDetails.getCodfuncpacote());
-			tabPedido.setCodfuncbalcao(tabPedidoDetails.getCodfuncbalcao());
-			tabPedido.setDatachegadacli(tabPedidoDetails.getDatachegadacli());
-			tabPedido.setDatafimbalcao(tabPedidoDetails.getDatafimbalcao());
-			tabPedido.setDatafimbalcao(tabPedidoDetails.getDatafimbalcao());
-			tabPedido.setDatafimsep(tabPedidoDetails.getDatafimsep());
-			tabPedido.setCodfuncpacote(tabPedidoDetails.getCodfuncpacote());
-			tabPedido.setEstoque(tabPedidoDetails.getEstoque());
-			tabPedido.setFinalizado(tabPedidoDetails.getFinalizado());
-			tabPedido.setOriginal(tabPedidoDetails.getOriginal());
-			tabPedido.setPainel(tabPedidoDetails.getPainel());
-			tabPedido.setRetira(tabPedidoDetails.getRetira());
-			tabPedido.setCliente(tabPedidoDetails.getCliente());
-			tabPedido.setStatus(tabPedidoDetails.getStatus());			
-			
-		  	final TabPedido updateTabPedido = tabPedidoRepository.save(tabPedido);
-		  	return ResponseEntity.ok(updateTabPedido);
-		   
-	   }
-	
-	 @DeleteMapping("/tabpedidos/{NUMPED}")
-	    public Map < String, Boolean > deleteTabPedido(@PathVariable(value = "NUMPED") BigDecimal NUMPED)
-	    throws ResourceNotFoundException {
-	        TabPedido tabPedido = tabPedidoRepository.findById(NUMPED)
-	            .orElseThrow(() -> new ResourceNotFoundException("Pedido não encontrado com esse Numped :: " + NUMPED));
+	public ResponseEntity<TabPedido> updateTabPedido(@PathVariable(value = "numped") BigDecimal numped,
+			@Valid @RequestBody TabPedido tabPedidoDetails) throws ResourceNotFoundException {
+		TabPedido tabPedido = tabPedidoRepository.findById(numped)
+				.orElseThrow(() -> new ResourceNotFoundException("Pedido não encontrado com esse Numped :: " + numped));
+		tabPedido.setCodfuncbalcao(tabPedidoDetails.getCodfuncbalcao());
+		tabPedido.setCodfuncpacote(tabPedidoDetails.getCodfuncpacote());
+		tabPedido.setCodfuncbalcao(tabPedidoDetails.getCodfuncbalcao());
+		tabPedido.setDatachegadacli(tabPedidoDetails.getDatachegadacli());
+		tabPedido.setDatafimbalcao(tabPedidoDetails.getDatafimbalcao());
+		tabPedido.setDatafimbalcao(tabPedidoDetails.getDatafimbalcao());
+		tabPedido.setDatafimsep(tabPedidoDetails.getDatafimsep());
+		tabPedido.setCodfuncpacote(tabPedidoDetails.getCodfuncpacote());
+		tabPedido.setEstoque(tabPedidoDetails.getEstoque());
+		tabPedido.setFinalizado(tabPedidoDetails.getFinalizado());
+		tabPedido.setOriginal(tabPedidoDetails.getOriginal());
+		tabPedido.setPainel(tabPedidoDetails.getPainel());
+		tabPedido.setRetira(tabPedidoDetails.getRetira());
+		tabPedido.setCliente(tabPedidoDetails.getCliente());
+		tabPedido.setStatus(tabPedidoDetails.getStatus());
 
-	        tabPedidoRepository.delete(tabPedido);
-	        Map < String, Boolean > response = new HashMap < > ();
-	        response.put("deleted", Boolean.TRUE);
-	        return response;
-	    }
+		final TabPedido updateTabPedido = tabPedidoRepository.save(tabPedido);
+		return ResponseEntity.ok(updateTabPedido);
+
+	}
+
+	@DeleteMapping("/tabpedidos/{NUMPED}")
+	public Map<String, Boolean> deleteTabPedido(@PathVariable(value = "NUMPED") BigDecimal NUMPED)
+			throws ResourceNotFoundException {
+		TabPedido tabPedido = tabPedidoRepository.findById(NUMPED)
+				.orElseThrow(() -> new ResourceNotFoundException("Pedido não encontrado com esse Numped :: " + NUMPED));
+
+		tabPedidoRepository.delete(tabPedido);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
 
 }
