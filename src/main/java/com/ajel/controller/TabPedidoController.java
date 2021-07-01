c995cbe333a5b88a3a088fd9333f66e93b512fb8
Douglas <douglasmariano.com@gmail.com>
2021-07-01 11:24:29 -0300

    erro no cors5

--- src/main/java/com/ajel/controller/TabPedidoController.java
+++ src/main/java/com/ajel/controller/TabPedidoController.java
@@ -29,6 +29,7 @@ import com.ajel.repository.TabPedidoRepository;
 import com.ajel.repository.filter.TabPedidosFilter;
 import com.ajel.services.TabPedidoService;
 
+@CrossOrigin(origins = "http://192.168.200.17:4200")
 @RestController
 @RequestMapping("/api/v1")
 public class TabPedidoController {
