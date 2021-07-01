c2f005b0ca8493434824e59321523b92f03d47d3
Douglas <douglasmariano.com@gmail.com>
2021-07-01 11:00:02 -0300

    erro no cors

--- src/main/java/com/ajel/controller/CorsConfiguration.java
+++ src/main/java/com/ajel/controller/CorsConfiguration.java
@@ -3,11 +3,21 @@ package com.ajel.controller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
-public class CorsConfiguration 
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer
 {
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**")
+               .allowedMethods("GET", "POST", "PUT", "DELETE");
+   }
+
+
     @Bean
     public WebMvcConfigurer corsConfigurer() 
     {
