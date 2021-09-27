1f5ed4ceb47b6d65f924e8f3e383bdeccf663a29
Diego Mariano <dieegorenan@gmail.com>
2021-09-27 21:42:50 +0200

    Merge branch 'main' of github.com:douglasmariano/restApiWinthor
    # Conflicts:
    #	src/main/java/com/ajel/controller/TabPedidoController.java
    #	target/classes/META-INF/MANIFEST.MF
    #	target/classes/META-INF/maven/com.ajel/restApiWinthor/pom.properties
    #	target/classes/application.properties
    #	target/classes/com/ajel/controller/CorsConfiguration$1.class
    #	target/classes/com/ajel/controller/EstoqueCaboController.class
    #	target/classes/com/ajel/controller/EstoqueController.class
    #	target/classes/com/ajel/controller/TabPedidoController.class
    #	target/classes/com/ajel/model/EstoqueCabo.class
    #	target/classes/com/ajel/repository/EstoqueCaboRepository.class
    #	target/classes/com/ajel/repository/estoquecabo/EstoqueCaboRepositoryQuery.class
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst
    #	target/restApiWinthor-1.0.0-SNAPSHOT.jar
    #	target/restApiWinthor-1.0.0-SNAPSHOT.jar.original

--- src/main/java/com/ajel/controller/payloads/UsuarioPayload.java
+++ src/main/java/com/ajel/controller/payloads/UsuarioPayload.java
@@ -1,31 +1,62 @@
 package com.ajel.controller.payloads;
 
-public class UsuarioPayload {
+import com.ajel.jwt.Role;
+import com.ajel.jwt.UserPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    private String usuario;
-    private String senha;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UsuarioPayload implements Authentication {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private boolean isAuthenticated;
+
+    public UsuarioPayload(Long id, String name, String username, String email, boolean isAuthenticated) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.isAuthenticated = isAuthenticated;
+    }
 
     @Override
-    public String toString() {
-        return "UsuarioPayload{" +
-                "usuario='" + usuario + '\'' +
-                ", senha='" + senha + '\'' +
-                '}';
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(Role.values());
     }
 
-    public String getUsuario() {
-        return usuario;
+    @Override
+    public Object getCredentials() {
+        return null;
     }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
+    @Override
+    public Object getDetails() {
+        return null;
     }
 
-    public String getSenha() {
-        return senha;
+    @Override
+    public Object getPrincipal() {
+        return new UserPrincipal(id, name, username, email, null, getAuthorities());
     }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
+    @Override
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean b) throws IllegalArgumentException {
+        this.isAuthenticated = b;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
